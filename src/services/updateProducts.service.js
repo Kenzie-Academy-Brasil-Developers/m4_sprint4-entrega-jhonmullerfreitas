1ab03d305c5c821787b8d5cacefda54958723a18
Jhon Muller <jmuller.jhon80@gmail.com>
2022-07-15 01:43:18 -0400

    fix: passando em todos os testes, ufaaaa

--- src/services/updateProducts.service.js
+++ src/services/updateProducts.service.js
@@ -3,6 +3,18 @@ import database from "../database";
 const updateProductsService = async (idProduct, name) => {
 
     try {
+        
+        const idProducts = await database.query(
+            "SELECT id FROM products"
+        );
+
+        const idProductExists = idProducts.rows.find((row) => row.id === idProduct);
+
+        if(idProductExists === undefined){
+            throw ("Este produto não existe.")
+        }
+        
+
         const res = await database.query(
             "UPDATE products SET name = ($2) WHERE id = ($1) RETURNING *",
             [idProduct, name]
